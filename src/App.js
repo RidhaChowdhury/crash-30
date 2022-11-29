46048fa7e9876008ecdcbf67c6349857a7eedfdd
Ridha Chowdhury <chowdhuryridha@gmail.com>
2022-11-29 00:33:56 -0500

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ function App() {
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
+          className="text-red-500"
         >
           Learn React
         </a>
